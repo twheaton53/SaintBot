ade7dd51502ca8761968e5e14b9a357082cc4e67
twheaton53 <traviswheaton53@gmail.com>
2021-06-30 11:32:52 -0700

    Implemented more complex command handler

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -5,9 +7,28 @@ const config = require('./config.json');
 const command = require('./command');
 const firstMessage = require('./first-message');
 const privateMessage = require('./private-message');
+const { dirname } = require('path');
 
 client.on('ready', () => {
     console.log('The client is ready')
+
+    const baseFile = 'commandBase.js';
+    const commandBase = require(`./commands/${baseFile}`);
+
+    const readCommands = dir => {
+        const files = fs.readdirSync(path.join(__dirname, dir));
+        for (const file of files) {
+            const stat = fs.lstatSync(path.join(__dirname, dir, file));
+            if (stat.isDirectory()) {
+                readCommands(path.join(dir, file));
+            } else if (file !== baseFile) {
+                const option = require(path.join(__dirname, dir, file));
+                commandBase(client, option);
+            }
+        }
+    }
+
+    readCommands('commands');
 
     firstMessage(client, '859503619477078036', 'hello world!!!', ['✨', '✌']);
     privateMessage(client, 'ping', 'Pong!');
